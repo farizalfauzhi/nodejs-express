2d4c2405bf011787c8084654f18662a832a5700f
gita gustia <farizalfauzi69@gmail.com>
2024-06-14 20:36:31 +0700

    install nodemon & basing routing dengan express

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+app.get('/', (req,res)=>{
+    res.send("<h2>Hello World</h2>")
+})
+
+app.get('/profil', (req,res)=>{
+    res.send("<h2>Profil saya</h2>")
+})
+
+app.get('/profesi', (req,res)=>{
+    res.send("<h2>Profesi saya programmer</h2>")
+})
+
+app.listen(port, ()=>{
+    console.log(`App sudah siap, buka http://localhost:${port}`)
+})
